1403cceb92bc7e1cc71564cd33c2c45a4ebba967
thodkatz <thodkatz@gmail.com>
2021-01-06 03:16:29 +0200

    Use dlmwrite with appropriate format
    Matlab output can be used to compare our C/C++ project impementation of kNN

--- knn.m
+++ knn.m
@@ -9,17 +9,5 @@ tic
 [idx, dist] = knnsearch(X, X, 'K', k, 'Distance', 'euclidean');
 toc
 
-file = fopen('matlab.txt', 'w');
-fprintf(file, '[');
-for k = 1:size(dist(:,1))
-    fprintf(file, '[%f,%f,%f,%f,%f],\n', dist(k,:));
-end
-fprintf(file, ']\n');
-
-fprintf(file, '[');
-for k = 1:size(idx(:,1))
-    fprintf(file, '[%d,%d,%d,%d,%d],\n', idx(k,:));
-end
-fprintf(file, ']\n');
-
-fclose(file);
+dlmwrite('matlab.txt', dist,'delimiter', ' ', 'precision', '%.06f');
+dlmwrite('matlab.txt', idx, '-append', 'delimiter', ' ');
